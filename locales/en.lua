5f46438ce6e18eecd9aa6e3bec3390eb46cc560b
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-02-03 22:33:13 +1100

    feat(locales/en): init `en` locale

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+return {
+  error = {
+    minimum_police = 'Minimum of %{value} police needed',
+    wrong_weapon = 'Your weapon is not strong enough..',
+    too_much = 'You\'re pockets are full..',
+    fail_therm = 'You failed to apply the thermite..',
+    fail_hack = 'You failed to hack the security system..',
+    skill_fail = 'Your %{value} skill is not high enough..'
+  },
+  success = {
+    thermite = 'Fuses blown, the doors should open soon..',
+    hacked = 'Hack successful, security is disabled..'
+  },
+  info = {
+    hacking_attempt = 'Connecting to the security system..',
+    close_warning = 'Hurry! The store will close in 1 minute'
+  },
+  general = {
+    store_label = 'Vangelico Jewellers',
+    target_label = 'Smash the display case',
+    thermite_label = 'Blow Fuse Box',
+    hack_label = 'Hack Security System'
+  }
+}
