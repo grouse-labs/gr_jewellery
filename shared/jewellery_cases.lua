e5be1f5106a995b84884803ac278781ab3fe6916
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-02-04 16:27:16 +1100

    feat(shared): init GigZ locations

--- shared/jewellery_cases.lua
+++ shared/jewellery_cases.lua
@@ -6,111 +6,176 @@ local hash_case_start_3 = `des_jewel_cab3_start`
 local hash_case_end_3 = `des_jewel_cab3_end`
 local hash_case_start_4 = `des_jewel_cab4_start`
 local hash_case_end_4 = `des_jewel_cab4_end`
-
+---@diagnostic disable: assign-type-mismatch
+---@type {[string]: {coords: vector3, heading: number, start_prop: integer, end_prop: integer}[]}
 return {
   main = {
     {
-      coords = vector3(-626.83, -235.35, 38.05),
+      coords = vector3(-627.21, -234.89, 37.65),
       heading = 36.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-625.81, -234.7, 38.05),
+      coords = vector3(-626.16, -234.13, 37.65),
       start_prop = hash_case_start_4,
       heading = 36.0,
       end_prop = hash_case_end_4
     }, {
-      coords = vector3(-626.95, -233.14, 38.05),
+      coords = vector3(-626.54, -233.6, 37.65),
       heading = 216.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-628.0, -233.86, 38.05),
+      coords = vector3(-627.59, -234.37, 37.65),
       heading = 216.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-625.7, -237.8, 38.05),
+      coords = vector3(-625.28, -238.29, 37.65),
       heading = 216.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-626.7, -238.58, 38.05),
+      coords = vector3(-626.33, -239.05, 37.65),
       heading = 216.0,
       start_prop = hash_case_start_2,
       end_prop = hash_case_end_2
     }, {
-      coords = vector3(-624.55, -231.06, 38.05),
-      heaing = 306.0,
+      coords = vector3(-623.96, -230.72, 37.65),
+      heaing = 216.0,
       start_prop = hash_case_start_4,
       end_prop = hash_case_end_4
     }, {
-      coords = vector3(-623.13, -232.94, 38.05),
+      coords = vector3(-622.62, -232.56, 37.65),
       heading = 306.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-620.29, -234.44, 38.05),
+      coords = vector3(-619.85, -234.91, 37.65),
       heading = 216.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-619.15, -233.66, 38.05),
+      coords = vector3(-618.8, -234.15, 37.65),
       heading = 216.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-620.19, -233.44, 38.05),
+      coords = vector3(-620.52, -232.88, 37.65),
       heading = 36.0,
       start_prop = hash_case_start_4,
       end_prop = hash_case_end_4
     }, {
-      coords = vector3(-617.63, -230.58, 38.05),
+      coords = vector3(-617.09, -230.16, 37.65),
       heading = 306.0,
       start_prop = hash_case_start_2,
       end_prop = hash_case_end_2
     }, {
-      coords = vector3(-618.33, -229.55, 38.05),
+      coords = vector3(-617.85, -229.11, 37.65),
       heading = 306.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-619.7, -230.33, 38.05),
+      coords = vector3(-620.18, -230.79, 37.65),
       heading = 126.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-620.95, -228.6, 38.05),
+      coords = vector3(-621.52, -228.95, 37.65),
       heading = 126.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-619.79, -227.6, 38.05),
+      coords = vector3(-619.2, -227.25, 37.65),
       heading = 306.0,
       start_prop = hash_case_start_2,
       end_prop = hash_case_end_2
     }, {
-      coords = vector3(-620.42, -226.6, 38.05),
+      coords = vector3(-619.97, -226.2, 37.65),
       heading = 306.0,
       start_prop = hash_case_start,
       end_prop = hash_case_end
     }, {
-      coords = vector3(-623.94, -227.18, 38.05),
+      coords = vector3(-624.28, -226.61, 37.65),
       heading = 36.0,
       start_prop = hash_case_start_4,
       end_prop = hash_case_end_4
     }, {
-      coords = vector3(-624.91, -227.87, 38.05),
+      coords = vector3(-625.33, -227.37, 37.65),
       heading = 36.0,
       start_prop = hash_case_start_3,
       end_prop = hash_case_end_3
     }, {
-      coords = vector3(-623.94, -228.05, 38.05),
+      coords = vector3(-623.61, -228.63, 37.65),
       heading = 216.0,
       start_prop = hash_case_start_2,
       end_prop = hash_case_end_2
     }
   },
-  grape = {},
-  paleto = {}
+  grape = {
+    {
+      coords = vector3(1646.35, 4879.66, 41.74),
+      heading = 98.18,
+      start_prop = hash_case_start_4,
+      end_prop = hash_case_end_4
+    }, {
+      coords = vector3(1647.6, 4881.69, 41.75),
+      heading = 8.18,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    }, {
+      coords = vector3(1648.79, 4882.52, 41.75),
+      heading = 188.18,
+      start_prop = hash_case_start,
+      end_prop = hash_case_end
+    }, {
+      coords = vector3(1647.5, 4882.33, 41.75),
+      heading = 188.18,
+      start_prop = hash_case_start_2,
+      end_prop = hash_case_end_2
+    }, {
+      coords = vector3(1648.66, 4886.42, 41.75),
+      heading = 8.18,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    }, {
+      coords = vector3(1649.94, 4886.6, 41.75),
+      heading = 8.18,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    },
+  },
+  paleto = {
+    {
+      coords = vector3(-379.13, 6049.96, 31.09),
+      heading = 314.91,
+      start_prop = hash_case_start_4,
+      end_prop = hash_case_end_4
+    }, {
+      coords = vector3(-378.91, 6047.59, 31.09),
+      heading = 224.91,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    }, {
+      coords = vector3(-379.37, 6046.21, 31.09),
+      heading = 44.91,
+      start_prop = hash_case_start,
+      end_prop = hash_case_end
+    }, {
+      coords = vector3(-378.45, 6047.13, 31.09),
+      heading = 44.91,
+      start_prop = hash_case_start_2,
+      end_prop = hash_case_end_2
+    }, {
+      coords = vector3(-376.94, 6043.16, 31.09),
+      heading = 224.91,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    }, {
+      coords = vector3(-377.85, 6042.25, 31.09),
+      heading = 224.91,
+      start_prop = hash_case_start_3,
+      end_prop = hash_case_end_3
+    }
+  }
 }
--- shared/store_locations.lua
+++ shared/store_locations.lua
@@ -1,6 +1,6 @@
+---@type {[string]: {coords: vector3, doors: string[], police: integer, thermite: {coords: vector3, heading: number, size: vector3}, hack: {coords: vector3, heading: number, size: vector3}?}}
 return {
   main = {
-    label = 'Vangelico\'s Jewellers',
     coords = vector3(-630.5, -237.13, 38.08),
     doors = {'jewellery-citymain', 'jewellery-citysec'},
     police = 0,
@@ -13,6 +13,26 @@ return {
       coords = vector3(-631.04, -230.63, 38.06),
       heading = 37.0,
       size = vector3(0.4, 0.6, 1.0)
+    }
+  },
+  grape = {
+    coords = vector3(1649.78, 4882.32, 42.16),
+    doors = {'jewellery-grapemain', 'jewellery-grapesec'},
+    police = 0,
+    thermite = {
+      coords = vector3(1645.07, 4867.87, 42.03),
+      heading = 8.0,
+      size = vector3(0.4, 0.8, 1.2)
+    }
+  },
+  paleto = {
+    coords = vector3(-378.45, 6047.68, 32.69),
+    doors = {'jewellery-palmain', 'jewellery-palsec'},
+    police = 0,
+    thermite = {
+      coords = vector3(-368.35, 6055.36, 31.5),
+      heading = 135.0,
+      size = vector3(0.4, 0.8, 1.2)
     }
   }
 }
