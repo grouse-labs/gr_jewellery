89211784b009d1585c26a3ca8daeca12fc23fa12
xGhostxDev <69668488+xGhostxDev@users.noreply.github.com>
2026-02-11 23:31:05 +0100

    feat(locales): add polish localization for various game messages (#2)
    * Add Polish localization for various game messages
    
    * Add debug messages to Polish localization

---
+++ locales/pl.lua
@@ -0,0 +1,34 @@
+return {
+  error = {
+    minimum_police = 'Minimalna ilość potrzbnej policij %{value}',
+    wrong_weapon = 'Twoja broń nie jest zbyt wytrzymała..',
+    too_much = 'Twój plecak jest pełny..',
+    fail_therm = 'Nie udało ci się odpalić termitu.',
+    fail_hack = 'Nie udało ci się włamać do systemu kamer..',
+    skill_fail = 'Poziom %{value} twoich umiejętności nie jest zbyt duży..'
+  },
+  success = {
+    thermite = 'Bezpieczniki wyłączone, za niedługo dzwi się otworzą..',
+    hacked = 'Udany hakowanie za chwilę system bezpieczeństwa będzie nie aktywny..'
+  },
+  info = {
+    hacking_attempt = 'Próba połączenia z systemem bezpieczeństwa..',
+    close_warning = 'Pośpiesz się! Sklep się zamknie za minutę'
+  },
+  general = {
+    store_label = 'Vangelico Jubiler', -- zakomentuj ta linie jeslis uzywasz wiecej jubilerow
+    -- store_labels = { -- odkometuj jesli uzywasz wiecej jubilerow
+    --   main = 'Little Portola Vangelico',
+    --   grape = 'Grapeseed Vangelico',
+    --   paleto = 'Paleto Vangelico'
+    -- },
+    target_label = 'Rozwal zabezpieczenie',
+    thermite_label = 'Wysadż bezpieczniki',
+    hack_label = 'Wyłącz alarm'
+  },
+  debug = {
+    enable = '%{version} - tryb debugowania %{state}!^7',
+    no_doors = 'Nie ma dzwi dla tej lokalizacji %{location}',
+    no_door_i = 'Nie ma dzwi #%<index>.d dla lokalizacji %{location}'
+  }
+}
